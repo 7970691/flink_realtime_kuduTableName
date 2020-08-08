0c9187522d2dd8e084b98c7ca5e4ce4a4d9f58ad
findczk <chenzk9706@gmail.com>
2020-08-08 16:54:46 +0800

    Initial commit

---
+++ src/main/java/com/xm4399/run/RealTimeIncrease2Kudu.java
@@ -0,0 +1,46 @@
+package com.xm4399.run;
+
+
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import com.xm4399.util.KafkaStringSchema;
+import com.xm4399.util.SubTableKuduSink;
+
+import java.util.Properties;
+
+public class RealTimeIncrease2Kudu {
+
+    public static void main(String[] args) {
+        realTimeIncrease(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+    }
+
+    public static void realTimeIncrease (String address, String username, String password, String dbName,
+                                   String tableName, String isSubTable, String isRealtime, String kuduTableName) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //  从kafka中读取数据
+        // 创建kafka相关的配置
+        Properties properties = new Properties();
+        //properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("bootstrap.servers", "10.0.0.194:9092,10.0.0.195:9092,10.0.0.199:9092");
+        properties.setProperty("group.id", "aa");
+        properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+        Properties props = new Properties();
+
+        //ConfArgsPojo confArgsPojo = new ConfArgsPojo(address, username, password, dbName, tableName, isSubTable, isRealtime);
+
+        DataStreamSink<ConsumerRecord<String,String>> stream = env
+                .addSource(new FlinkKafkaConsumer<ConsumerRecord<String,String>>(isRealtime, new KafkaStringSchema(), properties))
+                //.addSource(new FlinkKafkaConsumer<ConsumerRecord<String,String>>("qianduan_test", new KafkaStringSchema(), properties))
+                .addSink(new SubTableKuduSink(address, username, password, dbName, tableName, isSubTable, isRealtime, kuduTableName));
+        try {
+            env.execute();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/xm4399/util/ListAllSubTableName.java
@@ -0,0 +1,71 @@
+package com.xm4399.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+
+public class ListAllSubTableName {
+    //获取所有分表名
+    public  ArrayList<String> listAllSmallTableName(String address, String username, String password, String dbName, String tableName){
+        ArrayList<String> allSmallTableList = new ArrayList<String>() ;
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://" + address +"/" + dbName,
+                    username, password);
+
+            Statement stmt = con.createStatement();
+            //获取以tableName_开头的 后面跟着0-100  的所有表的表名
+            String sql = "select table_name from information_schema.tables where table_name REGEXP '"  + tableName + "_" + "[0-9]{1,3}';";
+            ResultSet res = stmt.executeQuery(sql);
+            while (res.next()) {
+                String smallTableName = res.getString(1);
+                System.out.println("将表  " + smallTableName +"  加入集合>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                allSmallTableList.add(smallTableName);
+
+            }
+            res.close();
+            stmt.close();
+            con.close();
+            return allSmallTableList;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return allSmallTableList;
+    }
+
+
+    //获取所有分表名
+    public static ArrayList<String> listAllSmallTableName2(String address, String username, String password, String  dbName, String tableName){
+        ArrayList<String> allSmallTableList = new ArrayList<String>() ;
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://" + address + "/" + dbName,
+                    username, password);
+            Statement stmt = con.createStatement();
+            //获取以tableName_开头的 后面跟着0-100  的所有表的表名
+            String sql = "select table_name from information_schema.tables where table_name REGEXP '"  + tableName + "_" + "[0-9]{1,3}';";
+            ResultSet res = stmt.executeQuery(sql);
+            while (res.next()) {
+                String smallTableName = res.getString(1);
+                System.out.println("将表  " + smallTableName +"  加入集合>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                allSmallTableList.add(smallTableName);
+            }
+            res.close();
+            stmt.close();
+            con.close();
+            return allSmallTableList;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return allSmallTableList;
+    }
+
+
+
+    }
+
